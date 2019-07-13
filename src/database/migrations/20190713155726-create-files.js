bbc97fcae15f5e07974d4e95f39990bd5b4debc0
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-13 13:47:10 -0300

    NEW - Usuário agora pode adicionar um avatar

---
+++ src/database/migrations/20190713155726-create-files.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('files', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      path: {
+        unique: true,
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('files');
+  },
+};
