491906e89dd6420607df9f5f0c37c80aadcaa7ff
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-14 23:29:15 -0300

    NEW - Envio de e-mail ao cancelar um agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1ad8f2400ed240',
+    pass: '3889ddee68e11c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
